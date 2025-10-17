c29423e2c7136bd5f484cb2d16ef4a34375faa24
Erik Anglemyer <erikanglemyer@gmail.com>
2025-10-17 10:54:45 -0400

    gg

--- Day_30_Drip.py
+++ Day_30_Drip.py
@@ -3,4 +3,11 @@ print("Welcome to the School Lunch Tracker!\nYou are managing the availablity of
 menu = "pizza: available, salad: low, sandwich: available, juice: unavailable"
 print("\nInitial Lunch Menu: ", menu)
 print("\nThe word 'salad' starts at index: ", menu.find("salad"))
-print("\nFirst two items on menu: ", menu[:menu.find("salad")-2])
+print("\nFirst two items on menu: ", (menu.split(", ")[0]))
+
+menu = menu.replace("salad: low", "salad: available")
+print("\nUpdated Lunch Menu after replacing 'salad': ", menu)
+print("\nLunch Menu in UPPERCASE: ", menu.upper)
+print("\nThe total number of characters in the lunch menu: ", len(menu))
+menu = menu + " fries: available"
+print("\nFinal Lunch Menu after adding 'fries': ", menu)
