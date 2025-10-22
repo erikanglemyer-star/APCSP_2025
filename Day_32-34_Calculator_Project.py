f3845e57f26ba52a2c604e7bd36f6eaca4f13848
Erik Anglemyer <erikanglemyer@gmail.com>
2025-10-22 14:06:00 -0400

    67

--- Day_32-34_Calculator_Project.py
+++ Day_32-34_Calculator_Project.py
@@ -1,10 +1,15 @@
+#Defines the wantStop variable
 wantStop = "no"
+
+#Prints a welcome message
 print("Welcome to the 2 Number Four Function Calculator!")
 
-while wantStop.lower() != "yes":
-    num1 = input("Please enter your first number (integer or decimal): ")
-    num2 = input("Please enter your second number (integer or decimal): ")
+#Main calculator loop
+while wantStop.lower() != "yes": #If the user doesn't say they want the program to stop, it will loop
+    #Prompts user to enter valid numbers
     while True:
+        num1 = input("Please enter your first number (integer or decimal): ")
+        num2 = input("Please enter your second number (integer or decimal): ")
         try:
             num1 = float(num1)
             num2 = float(num2)
@@ -12,12 +17,13 @@ while wantStop.lower() != "yes":
         except:
             print("Please enter valid numbers.")
             continue
+    #Performs Calculations
     print("The sum of the first and second number is: ", num1 + num2)
     print("The difference of the first and second number is: ", num1 - num2)
     print("The product of the first and second number is: ", num1 * num2)
-    if num2 == 0:
+    if num2 == 0: #stops division by 0 error
         print("Error: division by 0")
     else:
         print("The quotient of the first and second number is: ", num1 / num2)
         print("The remainder of the first and second number is: ", num1 % num2)
-    wantStop = input("Would you like to stop calculating? (type yes to end, anything else to continue): ")
+    wantStop = input("Would you like to stop calculating? (type yes to end, anything else to continue): ") 
