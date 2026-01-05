8efa094a64672bab0b951789aea2a19e4e572b08
Erik <erikanlemyer@gmail.com>
2026-01-05 08:06:36 -0500

    hehehehaq

--- Anglemyer_Erik_CarRentalProject.py
+++ Anglemyer_Erik_CarRentalProject.py
@@ -1,7 +1,21 @@
 print("Welcome to the Car Rental Charge Calculator!")
 
+def checkPosInt(check, command):
+    while True:
+        check = input("Please enter " + command + ": ")
+        try:
+            check = int(check)
+            if check > 0:
+                break
+            else:
+                print("Please enter a valid number of days.")
+        except:
+            print("Please enter a valid number of days.")
+    return check
+
 while True:
     code = ""
+    check = ""
     while code != "g" and code != "b" and code != "w" and code != "q":
         code = input("Please enter your car classification code (Q to quit): ").lower()
         if code == "g" or code == "b" or code == "w" or code == "q":
@@ -12,13 +26,18 @@ while True:
         break
     code = code.upper()
 
+    daysRented = checkPosInt(check, "the number of days rented")
+    
+    startMile = checkPosInt(check, "the odometer reading at start")
+
     while True:
-        daysRented = input("Please enter how many days the car was rented: ")
-        try:
-            daysRented = int(daysRented)
+        endMile = checkPosInt(check, "the odometer reading at end")
+        if endMile < startMile:
+            print("Please enter a higher ending mileage reading than starting.")
+        else:
             break
-        except:
-            print("Please enter a valid number of days.")
+
+
 
     
 
