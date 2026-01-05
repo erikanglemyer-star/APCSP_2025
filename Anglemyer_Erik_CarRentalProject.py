5922f29cf8fa418697f82456204eb1303a70c15e
erikanglemyer <erikanglemyer@gmail.com>
2026-01-05 08:48:16 -0500

    hehe

--- Anglemyer_Erik_CarRentalProject.py
+++ Anglemyer_Erik_CarRentalProject.py
@@ -8,17 +8,17 @@ def checkPosInt(check, command):
             if check > 0:
                 break
             else:
-                print("Please enter a valid number of days.")
+                print("Please enter a valid number.")
         except:
-            print("Please enter a valid number of days.")
+            print("Please enter a valid number.")
     return check
 
 while True:
     code = ""
     check = ""
-    while code != "g" and code != "b" and code != "w" and code != "q":
+    while code != "d" and code != "b" and code != "w" and code != "q":
         code = input("Please enter your car classification code (Q to quit): ").lower()
-        if code == "g" or code == "b" or code == "w" or code == "q":
+        if code == "d" or code == "b" or code == "w" or code == "q":
             break
         else:
             print("Please enter a valid code.")
@@ -26,7 +26,10 @@ while True:
         break
     code = code.upper()
 
-    daysRented = checkPosInt(check, "the number of days rented")
+    if code == "W":
+        weeksRented = checkPosInt(check, "the number of weeks rented")
+    else:
+        daysRented = checkPosInt(check, "the number of days rented")
     
     startMile = checkPosInt(check, "the odometer reading at start")
 
@@ -36,6 +39,37 @@ while True:
             print("Please enter a higher ending mileage reading than starting.")
         else:
             break
+    
+    miles = endMile - startMile
+    
+    print("Customer Classification code: ", code)
+
+    if code == "B":
+        baseCharge = 40 # per day
+        mileageCharge = 0.25 # per mile
+        total = daysRented * baseCharge + miles * mileageCharge
+        print("Rental period (days): ", daysRented)
+        print("Odometer reading at start: ", startMile)
+    
+    if code == "D":
+        baseCharge = 60 # per day
+        mileageCharge = 0.25 # Free for up to 100 miles per day, then per mile
+        freeMiles = 100 # per day
+        print("Rental period (days): ", daysRented)
+        print("Odometer reading at start: ", startMile)
+
+    if code == "W":
+        baseCharge = 190 # per week
+        if miles >= 900 and miles <= 1500:
+            mileageCharge = 0 
+            flatMileCharge = 100
+        elif miles > 1500:
+            flatMileCharge = 200
+            mileageCharge = 0.25 # For every mile past 1500
+        print("Rental period (weeks): ", weeksRented)
+        print("Odometer reading at start: ", startMile)
+    
+
 
 
 
