9c013963c8e1903e997a6eee0d585c008f5702ad
Erik <erikanlemyer@gmail.com>
2026-01-06 08:46:18 -0500

    Finished Code

--- Anglemyer_Erik_CarRentalProject.py
+++ Anglemyer_Erik_CarRentalProject.py
@@ -13,6 +13,19 @@ def checkPosInt(check, command):
             print("Please enter a valid number.")
     return check
 
+def checkPos(check, command):
+    while True:
+        check = input("Please enter " + command + ": ")
+        try:
+            check = float(check)
+            if check > 0:
+                break
+            else:
+                print("Please enter a valid number.")
+        except:
+            print("Please enter a valid number.")
+    return check
+
 while True:
     code = ""
     check = ""
@@ -31,17 +44,22 @@ while True:
         weeksRented = checkPosInt(check, "the number of weeks rented")
     else:
         daysRented = checkPosInt(check, "the number of days rented")
+        if daysRented == 1:
+            dayDays = "day"
+        else:
+            dayDays = "days"
     
-    startMile = checkPosInt(check, "the odometer reading at start")
+    startMile = checkPos(check, "the odometer reading at start")
 
     while True:
-        endMile = checkPosInt(check, "the odometer reading at end")
+        endMile = checkPos(check, "the odometer reading at end")
         if endMile < startMile:
             print("Please enter a higher ending mileage reading than starting.")
         else:
             break
     
     miles = endMile - startMile
+    
     
     print("Customer Classification code: ", code)
 
@@ -53,7 +71,7 @@ while True:
         print("Odometer reading at start:", startMile)
         print("Odometer reading at end:", endMile)
         print("Number of Miles Driven:", miles)
-        print("Base Charge: $" + str(baseCharge) + "/day x " + str(daysRented) + " days = $" + str(baseCharge * daysRented))
+        print("Base Charge: $" + str(baseCharge) + "/day x " + str(daysRented) + " " + dayDays + " = $" + str(baseCharge * daysRented))
         print("Mileage Charge: $" + str(mileageCharge) + "/mile x " + str(miles) + " miles = $" + str(mileageCharge * miles))
         print("Total Amount Due: $" + str(total))
 
@@ -68,19 +86,31 @@ while True:
         print("Odometer reading at start:", startMile)
         print("Odometer reading at end:", endMile)
         print("Number of Miles Driven:", miles)
-        print("Base Charge: $" + str(baseCharge) + "/day x " + str(daysRented) + " days = $" + str(baseCharge * daysRented))
+        print("Base Charge: $" + str(baseCharge) + "/day x " + str(daysRented) + " " + dayDays + " = $" + str(baseCharge * daysRented))
         print("Mileage Charge: First " + str(daysRented * freeMiles) + " miles free (100 miles/day), Extra " + str(paidMiles) + " miles x $" + str(mileageCharge) + " = $" + str(paidMiles * mileageCharge))
         print("Total Amount Due: $" + str(total))
 
     if code == "W":
         baseCharge = 190 # per week
-        if miles >= 900 and miles <= 1500:
+        baseMilesFree = 900 * weeksRented
+        maxMiles = 1500 * weeksRented
+        if miles >= baseMilesFree and miles <= maxMiles:
             mileageCharge = 0 
-            flatMileCharge = 100
-        elif miles > 1500:
-            flatMileCharge = 200
+            flatMileCharge = 100 * weeksRented
+        elif miles > maxMiles:
+            flatMileCharge = 200 * weeksRented
             mileageCharge = 0.25 # For every mile past 1500
-        print("Rental period (weeks): ", weeksRented)
-        print("Odometer reading at start: ", startMile)
-        print("Odometer reading at end: ", endMile)
-        print("Number of Miles Driven: ", miles)
+        if weeksRented == 1:
+            weekWeeks = "week"
+        else:
+            weekWeeks = "weeks"
+        total = (miles - maxMiles) * mileageCharge + weeksRented * baseCharge + flatMileCharge * weeksRented
+        print("Rental period (weeks):", weeksRented)
+        print("Odometer reading at start:", startMile)
+        print("Odometer reading at end:", endMile)
+        print("Number of Miles Driven:", miles)
+        print("Base Charge: $" + str(baseCharge) + "/week x " + str(weeksRented) + " " + weekWeeks + " = $" + str(weeksRented * baseCharge))
+        print("Mileage Charge: First " + str(baseMilesFree) + " miles free, Next " + str(600 * weeksRented) + " miles: $100.00 = " + str(flatMileCharge * weeksRented))
+        if miles > maxMiles:
+            print("Extra " + str(miles-maxMiles) + " miles ($0.25 per mile): " + str((miles - maxMiles) * mileageCharge))
+        print("Total Amount Due: $" + str(total))
