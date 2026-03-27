375850989f00c14b84a329ae5df028d9829b0893
Erik Anglemyer <erikanglemyer@gmail.com>
2026-03-27 13:41:06 -0400

    Big project simplification

--- UnitConverterPerformanceTask.py
+++ UnitConverterPerformanceTask.py
@@ -1,4 +1,5 @@
-units = ["feet", "yards", "miles", "meters", "kilometers", "centimeters", "inches"]
+print("Welcome to the Customary Unit Converter")
+units = ["feet", "yards", "miles", "inches"]
 unitConverter = 1
 finalUnit = 1
 
@@ -8,41 +9,35 @@ def checkUnit(unit):
             return unit
         else:
             print("Invalid unit entered.")
-            unit = input("Please input the distance unit you are converting (feet, yards, miles, meters, kilometers, centimeters):")
+            unit = input("Please input the customary distance unit you are converting (inches, feet, yards, miles): ")
 
-originalUnit = checkUnit(input("Please input the distance unit you are converting from (feet, yards, miles, meters, kilometers, centimeters): "))
+originalUnit = checkUnit(input("Please input the customary distance unit you are converting from (inches, feet, yards, miles): "))
 
 while True:
     unitAmount = input("Please enter the amount of the unit you would like to convert: ")
-    if unitAmount < 0:
-        print("Please enter a valid non-negative number.")
-    else:
-        try:
-            unitAmount = float(unitAmount)
-            break
-        except:
+    try:
+        unitAmount = float(unitAmount)
+        if unitAmount < 0:
             print("Please enter a valid non-negative number.")
+        else:
+            break
+    except:
+        print("Please enter a valid non-negative number.")
 
-newUnit = checkUnit(input("Please input the distance unit you are converting to (feet, yards, miles, meters, kilometers, centimeters): "))
+newUnit = checkUnit(input("Please input the customary distance unit you are converting to (inches, feet, yards, miles): "))
 
 if originalUnit == "yards":
-    unitConverter = unitAmount / 3
+    unitConverter = unitAmount * 3
 elif originalUnit == "inches":
-    unitConverter = unitAmount * 12
-elif originalUnit == "meters":
-    unitConverter = unitAmount * 0.3048
-elif originalUnit == "centimeters":
-    unitConverter = unitAmount * 30.48
+    unitConverter = unitAmount / 12
 elif originalUnit == "miles":
     unitConverter = unitAmount * 5280
 
 if newUnit == "yards":
-    finalUnit = unitConverter * 3
+    finalUnit = unitConverter / 3
 elif newUnit == "inches":
-    finalUnit = unitAmount / 12
-elif newUnit == "meters":
-    finalUnit = unitAmount / 0.3048
-elif newUnit == "centimeters":
-    finalUnit = unitAmount / 30.48
+    finalUnit = unitAmount * 12
 elif newUnit == "miles":
     finalUnit = unitAmount / 5280
+
+print(str(unitAmount) + " " + originalUnit + " converts to " + str(finalUnit) + " " + newUnit + ".")
