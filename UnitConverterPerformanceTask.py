5bc8ff97a3017f3f5bfe4d15381ff963da81918f
Erik Anglemyer <erikanglemyer@gmail.com>
2026-04-10 14:04:31 -0400

    Finished code?

--- UnitConverterPerformanceTask.py
+++ UnitConverterPerformanceTask.py
@@ -3,7 +3,6 @@ print("Welcome to the Customary Unit Converter") # Welcome the user
 # These lists store units and their conversions, allowing for the user to easily add more conversions without breaking the algorithm.
 units = ["inches", "feet", "yards", "miles"]
 feetConversion = [1/12, 1, 3, 5280]
-shouldQuit = "yes"
 
 # Function that checks to ensure the unit entered is a customary distance unit
 def checkUnit(unit):
@@ -27,31 +26,29 @@ def convertUnit(quantity, originalUnit, newUnit):
 
     return finalQuantity
 
-while shouldQuit != "no":
-    # User inputs which unit they are converting from
-    originalUnit = checkUnit(input("Please input the customary distance unit you are converting from (inches, feet, yards, miles): ").lower())
+# User inputs which unit they are converting from
+originalUnit = checkUnit(input("Please input the customary distance unit you are converting from (inches, feet, yards, miles): ").lower())
 
-    # Collect the Quantity of the unit the user is converting
-    while True: 
-        originalQuantity = input("Please enter the quantity of the unit you would like to convert: ") # Collects user input
-        try: # Checks to make sure it is a non-negative number
-            originalQuantity = float(originalQuantity)
-            if originalQuantity < 0:
-                print("Please enter a valid non-negative number.")
-            else:
-                break
-        except:
+# Collect the Quantity of the unit the user is converting
+while True: 
+    originalQuantity = input("Please enter the quantity of the unit you would like to convert: ") # Collects user input
+    try: # Checks to make sure it is a non-negative number
+        originalQuantity = float(originalQuantity)
+        if originalQuantity < 0:
             print("Please enter a valid non-negative number.")
+        else:
+            break
+    except:
+        print("Please enter a valid non-negative number.")
 
-    # User inputs which unit they are converting to
-    newUnit = checkUnit(input("Please input the customary distance unit you are converting to (inches, feet, yards, miles): ").lower())
+# User inputs which unit they are converting to
+newUnit = checkUnit(input("Please input the customary distance unit you are converting to (inches, feet, yards, miles): ").lower())
 
-    # Call the function to convert the unit
-    convertedQuantity = convertUnit(originalQuantity, originalUnit, newUnit)
+# Call the function to convert the unit
+convertedQuantity = convertUnit(originalQuantity, originalUnit, newUnit)
 
-    # Print final calculation
-    print(str(originalQuantity) + " " + originalUnit + " converts to " + str(convertedQuantity) + " " + newUnit + ".")
-    shouldQuit = input("Would you like to calculate another unit conversion? (Type 'no' to quit): ").lower()
+# Print final calculation
+print(str(originalQuantity) + " " + originalUnit + " converts to " + str(convertedQuantity) + " " + newUnit + ".")
 
 
 
